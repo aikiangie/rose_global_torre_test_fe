91284f0032c1a3914a0ed3db1d1e49f7986f501a
Luz Angelica Jiménez Rozo <luz.jimenez.consultant@sothebys.com>
2019-08-08 11:51:43 -0500

    Missing crud actions added to service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,40 @@ import { Observable, of } from 'rxjs';
 export class UserService {
 
   private apiUrl = 'http://langelicajr.pythonanywhere.com/users/';
+  headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
+  httpOptions = {
+    headers: this.headers
+  };
+
+  private handleError(error: any) {
+    console.error(error);
+    return throwError(error);
+  }
 
   getUsers(): Observable<User[]> {
     return this.httpClient.get<User[]>(this.apiUrl);
+  }
+
+  addUser(user: User): Observable<User> {
+    user.id = null;
+    return this.httpClient.post<User>(this.apiUrl, user, this.httpOptions).pipe(
+      tap(data => console.log(data)),
+      catchError(this.handleError)
+    );
+  }
+
+  deleteUser(id: number): Observable<User> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.httpClient.delete<User>(url, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  updateUser(user: User): Observable<User> {
+    const url = `${this.apiUrl}/${user.id}`;
+    return this.httpClient.put<User>(this.apiUrl, user, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
   }
 
   constructor(private httpClient: HttpClient) { }
